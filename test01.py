099151d8a223f7690758eb85c194c4f027cf7e54
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2010-01-27 05:40:10 -0800

    Fighting with Model, globals() type list, and whatnot

--- test01.py
+++ test01.py
@@ -1,7 +1,7 @@
-from Model import *
-import setup00
+from Model import Model
+from setup00 import *
 
-products = Model.get('Product')
+products = Model.get(Product)
 
 p = products[0]
 print('Product ' + p.id)
